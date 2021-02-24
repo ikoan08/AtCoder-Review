7ccf452af224077e79430c720c204ec2479869ab
Naoki <36870844+Naoki08@users.noreply.github.com>
2021-02-24 14:01:54 +0900

    package変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/header_icon.jpeg' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
